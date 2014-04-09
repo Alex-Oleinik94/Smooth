488ab470e71ef8b93eb7a4dc808e6980a4c0c9bd
Sancho <myfirststrangeemail@gmail.com>
2014-04-09 20:12:19 +0400

    Правки Ex6.

--- Examples/Example6/Ex6_2.pp
+++ Examples/Example6/Ex6_2.pp
@@ -1,5 +1,5 @@
 {$INCLUDE SaGe.inc}
-program Example6;
+program Example6_2;
 uses
 	{$IFDEF UNIX}
 		{$IFNDEF ANDROID}
@@ -31,20 +31,29 @@ type
 		
 		FSunAngle : TSGSingle;
 		FImageBump, FImageTexture : TSGImage;
+		FSunRadius : TSGSingle;
 		end;
 
 class function TSGExample.ClassName():TSGString;
 begin
-Result := 'Bump Mapping';
+Result := 'Bump Mapping 2';
 end;
 
 constructor TSGExample.Create(const VContext : TSGContext);
+var
+	i : TSGLongWord;
+	r : TSGSingle = 2;
+	a : TSGSingle = pi * 2;
+	n : TSGByte    = 20;
+	t : TSGSingle;
 begin
 inherited Create(VContext);
 FCamera:=TSGCamera.Create();
 FCamera.SetContext(Context);
 FMesh := nil;
-FSunAngle:=0;
+FSunAngle  := 0;
+FSunRadius := r * 0.6;
+t := n/a;
 
 FMesh := TSGCustomModel.Create();
 FMesh.Context := Context;
@@ -55,25 +64,32 @@ FMesh.LastObject().HasNormals := True;
 FMesh.LastObject().HasTexture := True;
 FMesh.LastObject().HasColors  := False;
 FMesh.LastObject().VertexType := SGMeshVertexType3f;
-FMesh.LastObject().Vertexes   := 4;
-FMesh.LastObject().ArVertex3f[0]^.Import(-1,-1);
-FMesh.LastObject().ArVertex3f[1]^.Import(-1,1);
-FMesh.LastObject().ArVertex3f[2]^.Import(1,1);
-FMesh.LastObject().ArVertex3f[3]^.Import(1,-1);
+FMesh.LastObject().Vertexes   := n * 2;
+
+FMesh.LastObject().AddFaceArray();
+FMesh.LastObject().AutoSetIndexFormat(0, n * 2);
+FMesh.LastObject().PoligonesType     [0] := SGR_TRIANGLES;
+FMesh.LastObject().Faces             [0] := (n - 1) * 2;
+
+FMesh.LastObject().ArVertex3f [0]^.Import(cos(0)*r,sin(0)*r, 1);
+FMesh.LastObject().ArVertex3f [1]^.Import(cos(0)*r,sin(0)*r,-1);
 FMesh.LastObject().ArTexVertex[0]^.Import(0,0);
 FMesh.LastObject().ArTexVertex[1]^.Import(0,1);
-FMesh.LastObject().ArTexVertex[2]^.Import(1,1);
-FMesh.LastObject().ArTexVertex[3]^.Import(1,0);
-FMesh.LastObject().ArNormal[0]^.Import(0,0,1);
-FMesh.LastObject().ArNormal[1]^.Import(0,0,1);
-FMesh.LastObject().ArNormal[2]^.Import(0,0,1);
-FMesh.LastObject().ArNormal[3]^.Import(0,0,1);
-FMesh.LastObject().AddFaceArray();
-FMesh.LastObject().AutoSetIndexFormat(0,4);
-FMesh.LastObject().PoligonesType[0] := SGR_TRIANGLES;
-FMesh.LastObject().Faces [0] := 2;
-FMesh.LastObject().SetFaceTriangle(0,  0,  0,1,2);
-FMesh.LastObject().SetFaceTriangle(0,  1,  0,2,3);
+FMesh.LastObject().ArNormal   [0]^.Import(-cos(0),-sin(0),0);
+FMesh.LastObject().ArNormal   [1]^.Import(-cos(0),-sin(0),0);
+
+for i := 1 to n - 1 do
+	begin
+	FMesh.LastObject().ArVertex3f [i*2+0]^.Import(cos(a*i/(n-1))*r,sin(a*i/(n-1))*r, 1);
+	FMesh.LastObject().ArVertex3f [i*2+1]^.Import(cos(a*i/(n-1))*r,sin(a*i/(n-1))*r,-1);
+	FMesh.LastObject().ArTexVertex[i*2+0]^.Import(i/(n-1)*t,0);
+	FMesh.LastObject().ArTexVertex[i*2+1]^.Import(i/(n-1)*t,1);
+	FMesh.LastObject().ArNormal   [i*2+0]^.Import(-cos(a*i/(n-1)),-sin(a*i/(n-1)),0);
+	FMesh.LastObject().ArNormal   [i*2+1]^.Import(-cos(a*i/(n-1)),-sin(a*i/(n-1)),0);
+	
+	FMesh.LastObject().SetFaceTriangle(0, (i - 1)*2+0, i*2+0,i*2-2,i*2-1);
+	FMesh.LastObject().SetFaceTriangle(0, (i - 1)*2+1, i*2+0,i*2-1,i*2+1);
+	end;
 
 FMesh.AddMaterial ().Name := 'name';
 FMesh.LastMaterial().AddDiffuseMap('Ex6_D.jpg');
@@ -85,6 +101,8 @@ FMesh.LoadToVBO();
 
 FImageBump    := FMesh.LastMaterial().ImageBump;
 FImageTexture := FMesh.LastMaterial().ImageTexture;
+
+//FMesh.WriteInfo();
 end;
 
 destructor TSGExample.Destroy();
@@ -118,7 +136,7 @@ begin
 FCamera.CallAction();
 
 FSunAngle += Context.ElapsedTime*0.01;
-FSun.Import(cos(FSunAngle)*2,sin(FSunAngle)*2,2);
+FSun.Import(cos(FSunAngle)*FSunRadius,sin(FSunAngle)*FSunRadius);
 
 Render.Color3f(1,1,1);
 Render.BeginScene(SGR_POINTS);
@@ -126,8 +144,8 @@ FSun.Vertex(Render);
 Render.EndScene();
 
 Render.Disable(SGR_BLEND);
-Render.Enable(SGR_LIGHTING);
-Render.Enable(SGR_LIGHT0);
+Render.Enable (SGR_LIGHTING);
+Render.Enable (SGR_LIGHT0);
 Render.Lightfv(SGR_LIGHT0, SGR_POSITION, @FSun);
 
 if FMesh.LastMaterial().EnableBump then
@@ -138,7 +156,7 @@ FMesh.Draw();
 if FMesh.LastMaterial().EnableBump then
 	Render.EndBumpMapping();
 
-Render.Enable(SGR_BLEND);
+Render.Enable (SGR_BLEND);
 Render.Disable(SGR_LIGHTING);
 Render.Disable(SGR_LIGHT0);
 
