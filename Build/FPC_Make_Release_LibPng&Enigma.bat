00f6ee5753bf19d944fa62c9d60680edc2aa54b4
Alexander <alex.olein@yandex.ru>
2016-05-30 16:01:34 +0300

    Enigma&LibPng Release fixes

--- Build/FPC_Make_Release_LibPng&Enigma.bat
+++ Build/FPC_Make_Release_LibPng&Enigma.bat
@@ -7,7 +7,13 @@ MKDIR CompiledUnits\i386-other
 make build_files
 make inc_version_release
 @echo off
-make release
+make release_libpng
 @echo off
 make clear_files
+@echo off
+echo "============================================="
+echo "|Сейчас вылетит программа "enigmavbconsole",|"
+echo "| но она сделает свою работу нормально      |"
+echo "============================================="
+"./EnigmaVB/enigmavbconsole.exe" Main.evb
 pause
