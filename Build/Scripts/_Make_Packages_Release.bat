03a74938d37018528d54b5126a39cf53dbdeb0b6
Alexander <alex.olein@yandex.ru>
2016-12-25 11:57:00 +0300

    Default package build script.

--- Build/Scripts/_Make_Packages_Release.bat
+++ Build/Scripts/_Make_Packages_Release.bat
@@ -1,15 +1,3 @@
 @echo off
-CALL _Create_Output_Directoryes
-cd ..
-set S1="=============================="
-set S2="Compiling Packages for Windows"
-set S3="=============================="
-echo %S1%
-echo %S2%
-echo %S3%
-"../Binaries/Main_Console.exe" --build release
-if %errorlevel% equ 0 (
-	copy .\..\Binaries\Main.exe .\..\Binaries\Main_Packages.exe
-	)
-cd Scripts
+CALL _Make_Packages release false
 if "%1"=="" ( pause )
