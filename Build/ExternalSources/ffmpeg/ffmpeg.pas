0dff888205d7e1951fd9ae91ea7c4ccb448609b6
Alexander <alex.olein@yandex.ru>
2017-01-05 23:35:13 +0300

    Add FFMPEG.

---
+++ Build/ExternalSources/ffmpeg/ffmpeg.pas
@@ -0,0 +1,39 @@
+
+unit ffmpeg;
+
+interface
+
+uses
+	ffm.libavcodec.avcodec,
+	//ffm.avfilter,
+	ffm.avformat,
+	ffm.avio,
+	ffm.avutil,
+	ffm.buffer,
+	//ffm.buffersink,
+	//ffm.buffersrc,
+	ffm.channel_layout,
+	ffm.cls.videoencoder,
+	ffm.ctypes,
+	ffm.dict,
+	ffm.errno,
+	ffm.error,
+	ffm.frame,
+	ffm.imgutils,
+	ffm.lib,
+	ffm.log,
+	ffm.mathematics,
+	ffm.mem,
+	ffm.opt,
+	ffm.parseutils,
+	ffm.pixdesc,
+	ffm.pixfmt,
+	ffm.rational,
+	ffm.samplefmt,
+	ffm.swresample,
+	ffm.swscale,
+	ffm.time
+	;
+implementation
+
+end.
