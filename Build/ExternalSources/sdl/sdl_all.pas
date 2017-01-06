e7b5f440dfd2ab798ad4091e67ed1d865de4f780
Alexander <alex.olein@yandex.ru>
2017-01-06 23:36:00 +0300

    Added SDL and SDL2.

---
+++ Build/ExternalSources/sdl/sdl_all.pas
@@ -0,0 +1,26 @@
+unit sdl_all;
+
+interface
+
+uses
+	sdl,
+	fastevents,
+	libxmlparser,
+	logger,
+	moduleloader,
+	registryuserpreferences,
+	sdl_cpuinfo,
+	sdlgameinterface,
+	sdli386utils,
+	sdlinput,
+	sdlstreams,
+	sdlticks,
+	sdlutils,
+	sdlwindow,
+	userpreferences,
+	xplatformutils
+	;
+
+implementation
+
+end.
---
+++ Build/ExternalSources/sdl2/sdl2_all.pas
@@ -0,0 +1,15 @@
+unit sdl2_all;
+
+interface
+
+uses
+	sdl2
+	,SDL2_Mixer
+	,SDL2_Net
+	,SDL2_Ttf
+	,SDL2_Image
+	;
+
+implementation
+
+end.
